7a5745f12b7bb90de709aee753013e67a2fdab26
vinay_hv <vinayhv66@gmail.com>
2025-10-06 01:17:03 +0530

    styled app deploy 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-    // vite.config.js
-    import { defineConfig } from 'vite';
-    import react from '@vitejs/plugin-react';
-    import tailwindcss from '@tailwindcss/vite'; // Import the plugin
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // Import the plugin
 
-    export default defineConfig({
-      plugins: [
-        react(),
-        tailwindcss(), // Add the Tailwind CSS plugin
-      ],
-    });
+export default defineConfig({
+  base: './', // use relative asset paths so /assets/... doesn't 404 in static hosting
+  plugins: [
+    react(),
+    tailwindcss(), // Add the Tailwind CSS plugin
+  ],
+});
